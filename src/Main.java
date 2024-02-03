1668b815d0867a982e820ab5598d2096243f0582
LukeAWoodruff <woodruffluke0@gmail.com>
2024-02-02 19:27:55 -0500

    V3

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main
         final double INTREST_RATE = 1.17;
        cardBalance = cardBalance * INTREST_RATE;
         System.out.println("The card balance due after one month is: " + cardBalance);
+
         cardBalance = cardBalance * INTREST_RATE;
         System.out.println("The card balance due after two months is: " + cardBalance);
 
